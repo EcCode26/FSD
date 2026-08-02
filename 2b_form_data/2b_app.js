14a1e053790f8a4126128b16508795a2dcf2b898
EcCode26 <eccode94@gmail.com>
2026-08-02 07:45:24 +0530

    2b_form_data

---
+++ 2b_form_data/2b_app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Middleware to read form data
+app.use(express.urlencoded({ extended: true }));
+
+// Configure EJS
+app.set("view engine", "ejs");
+
+// Home Page
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+// Open Registration Form
+app.get("/register", (req, res) => {
+    res.render("form");
+});
+
+// Receive Form Data
+app.post("/register", (req, res) => {
+    res.render("result", {
+        student: req.body
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
